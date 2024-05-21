5989e0e3b81aa8c181f0baaee59231be1be54173
shady <shady@shady.speedport.ip>
2024-05-21 23:45:13 +0200

    change carousel component

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -2,14 +2,14 @@ import axios from "axios";
 import CarouselItem from "./CarouselItem";
 import "../styles/carousel.css";
 export async function getCarouselElements() {
-  const uri = "http://localhost:3000/api/products";
-  const api = await axios.get(uri);
-  const data = api.data;
-  return data;
+  // const uri = "http://localhost:3000/api/products";
+  // const api = await axios.get(uri);
+  // const data = api.data;
+  // return data;
 }
 
 export default async function Carousel() {
-  const element = await getCarouselElements();
+  // const element = await getCarouselElements();
 
   return (
     <div className="carousel-container">
