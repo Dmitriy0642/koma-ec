6d8ee0800b1e60daf4797c8605d327d4c39ed1d7
shady <shady@shady.speedport.ip>
2024-05-22 19:18:10 +0200

    made cors

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,4 @@
-export const BASE_URL: string =
-  "https://koma-1q594puc1-dmitriy0642s-projects.vercel.app";
-// https://koma-ec.vercel.app/api
+export const BASE_URL: string = "https://koma-ec.vercel.app/api/";
+// https://koma-ec.vercel.app/
 // http://localhost:3000/api/
 // https://koma-1q594puc1-dmitriy0642s-projects.vercel.app'
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
