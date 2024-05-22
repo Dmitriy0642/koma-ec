fcb17b6c1b35041065a50818352ee609c8fb94bc
shady <shady@shady.speedport.ip>
2024-05-22 23:17:51 +0200

    change url

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,5 @@
-export const BASE_URL: string = "https://koma-ec.vercel.app/api";
+export const BASE_URL: string =
+  "https://koma-ec-git-development-dmitriy0642s-projects.vercel.app/api";
 // https://koma-ec.vercel.app/
 // http://localhost:3000/api/
 // https://koma-ec-git-main-dmitriy0642s-projects.vercel.app/
