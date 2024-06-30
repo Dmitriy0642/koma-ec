a0d64b88b18664961aaa04b7c87608ee3523b90e
shady <shady0642@gmail.com>
2024-06-30 19:02:56 +0200

    see on id at production

--- app/hooks/useRequestById.tsx
+++ app/hooks/useRequestById.tsx
@@ -2,10 +2,13 @@
 import axios from "axios";
 import { BASE_URL } from "../config";
 import { useQuery } from "@tanstack/react-query";
+import { getCookieValue } from "../util/cookiesMatcher";
 
 export const useRequestById = (collection: string, id: string) => {
   const fetchData = async () => {
     if (id.length !== 0) {
+      const uId = getCookieValue();
+      console.log(uId);
       console.log(id);
 
       const { data } = await axios.get(`${BASE_URL}/${collection}/${id}`);
